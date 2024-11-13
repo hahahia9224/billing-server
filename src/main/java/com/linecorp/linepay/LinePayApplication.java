3372ddc1a3621edc1bb35a6dd7bef5b4ef837694
hahahia9224 <hahahia9224@gmail.com>
2024-11-14 04:19:25 +0900

    git init

---
+++ src/main/java/com/linecorp/linepay/LinePayApplication.java
@@ -0,0 +1,13 @@
+package com.linecorp.linepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinePayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinePayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linecorp/linepay/LinePayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linecorp.linepay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinePayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
