19aa599adb42b78e63221050170e9b0bcf536d87
hahahia9224 <hahahia9224@gmail.com>
2024-11-14 05:29:24 +0900

    Request 객체 주석 추가

--- src/main/java/com/linecorp/linepay/model/PaymentRequest.java
+++ src/main/java/com/linecorp/linepay/model/PaymentRequest.java
@@ -9,9 +9,12 @@ import java.util.List;
 @Setter
 public class PaymentRequest {
 
+    // 결제 금액
     private Integer amount;
 
+    // 결제 제목
     private String title;
 
+    // 프로모션 정보 리스트
     private List<PromotionRequest> promotions;
 }
--- src/main/java/com/linecorp/linepay/model/PromotionRequest.java
+++ src/main/java/com/linecorp/linepay/model/PromotionRequest.java
@@ -7,11 +7,15 @@ import lombok.Setter;
 @Setter
 public class PromotionRequest {
 
+    // 프로모션 유형
     private String promotionType;
 
+    // 프로모션 제목
     private String promotionTitle;
 
+    // 프로모션 할인 금액
     private Integer promotionAmount;
 
+    // 프로모션 할인 비율
     private Float promotionRatio;
 }
